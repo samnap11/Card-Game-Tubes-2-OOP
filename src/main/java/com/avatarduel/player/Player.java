7c0b0dd3369576ea52bec3588b0bb336b9b60f0e
dvnvln <50177892+dvnvln@users.noreply.github.com>
2020-04-20 07:48:24 +0700

    comment

--- src/main/java/com/avatarduel/player/Player.java
+++ src/main/java/com/avatarduel/player/Player.java
@@ -17,6 +17,10 @@ import com.avatarduel.state.State;
 /**
  * Representation of a Player
  */
+/**
+ * Player is the component based player who play the game
+ * @author Devin
+ */
 public class Player {
     private int hp;
     protected ArrayList<Card> hand;
@@ -27,6 +31,9 @@ public class Player {
     private List<Integer> powerUp;
     private HBox healthBar = new HBox();
 
+    /**
+     * Default constuctor of player
+     */
     public Player(){
         this.hp = 80;
         this.hand = new ArrayList<>();
@@ -42,77 +49,138 @@ public class Player {
         this.element.put(Element.ENERGY,new Pair<>(1,1));
     }
     
+    /**
+     * delete card on the hand at index idx
+     * @param idx
+     */
     public void removeHand(int idx){
         hand.set(idx,null);
         hand.remove(null);
         HandView.initHand(this);
     }
-
+    /**
+     * getter for the element of the card
+     * @return element the element of the card
+     */
     public Map<Element,Pair<Integer,Integer>> getElement(){
         return element;
     }   
 
+    /**
+     * getter for the summoned cards
+     * @return cards that have summoned before
+     */
     public Map<Integer,Card> getSummonedCards(){
         return summonedCards;
     }
-
+    
+    /**
+     * getter for the card info
+     * @return card info
+     */
     public Map<Integer,Pair<Boolean,Boolean>> getCardInfo(){
         return cardInfo;
     }  
 
+
+    /**
+     * getter powerup
+     * @return powerup
+     */
     public List<Integer> getPowerUp(){
         return powerUp;
     }
 
+    /**
+     * getter deck of cards
+     * @return deck of cards
+     */
     public Deck getDeck(){
         return pDeck;
     }                        
                     
-
+    /**
+     * getter health points
+     * @return health points
+     */
     public int getHp(){
         return this.hp;
     }
 
+    /**
+     * setter health points
+     * @param hp
+     */
     public void setHp(int hp){
         this.hp = hp;
     }
 
+    /**
+     * take card from the deck and put it on the hand
+     */
     public void takeCard(){
         Card now = pDeck.Draw();
         hand.add(now);
         FieldSide.updateDeckSize(this);
     }
 
+    /**
+     * input arraylist of cards into deck
+     * @param a
+     */
     public void fillDeck(ArrayList<Card> a){
         pDeck.inputDeck(a);
     }
 
+    /**
+     * add element according to each type of element
+     * @param el
+     * @param x
+     */
     public void addElement(Element el,int x){
         Pair<Integer,Integer> temp = element.get(el);
         element.put(el,new Pair<>(temp.getValue()+x,temp.getValue()+x));
     }
 
+    /**
+     * reset element
+     */
     public void resetElement() {
         element.forEach((el,per)->{
             element.put(el, new Pair<>(per.getValue(),per.getValue()));
         });
     }
 
+    /**
+     * getter card on the hand at index x
+     * @param x
+     * @return card or null
+     */
     public Card peekCard(int x){
         // int x = this == State.p1 ? idx : idx - 10;
         if (hand.size() > x) return hand.get(x);
         return null;
     }
 
+    /**
+     * getter hand
+     * @return hand with cards or without
+     */
     public ArrayList<Card> getHand(){
         return hand;
     }
 
+    /**
+     * getter size of hand
+     * @return size of hand
+     */
     public int sizeHand(){
         return hand.size();
     }
 
-
+    /**
+     * drawphase
+     */
     public void turnInit(){
         if (sizeHand() < 10)
             takeCard();
@@ -120,12 +188,19 @@ public class Player {
         resetInfo();
     }
 
+    /**
+     * reset card info
+     */
     public void resetInfo(){
         for (int idx : cardInfo.keySet()){
             cardInfo.put(idx,new Pair<>(false,cardInfo.get(idx).getValue()));
         }
     }
 
+    /**
+     * place card on the field
+     * @param idx index card at the field 
+     */
     public void placeAtField(int idx){
         int click = this == State.p1 ? State.clickHand : State.clickHand - 10;
         element.put(peekCard(click).getElement(),new Pair<>(element.get(peekCard(click).getElement()).getKey() - ((com.avatarduel.card.Character) peekCard(click)).getCost(),element.get(peekCard(click).getElement()).getValue()));
@@ -137,6 +212,10 @@ public class Player {
         FieldSide.updateLand(this);
     }
     
+    /**
+     * use skill card
+     * @param idx index card
+     */
     public void useSkill(int idx){
         int click = State.checkTurn(State.p1) ? State.clickHand : State.clickHand - 10;
         Player p = idx > 11 ? State.p1 : State.p2;
@@ -158,15 +237,28 @@ public class Player {
         FieldView.initField(this);
     }
 
+    /**
+     * delete card cx on the hand
+     * @param cx card
+     */
     public void removeHandCard(Card cx){
         if (hand.contains(cx)) hand.remove(hand.indexOf(cx));
     }
 
+    /**
+     * getter available amount of element and total amount of element
+     * @param e element of card
+     * @return
+     */
     public String getLand(Element e){
         // Pair<int,int> = element.get(e);
         return String.format("%d/%d",element.get(e).getKey(),element.get(e).getValue());
     }
 
+    /**
+     * delete card on the field at index idx
+     * @param idx index card on the field
+     */
     public void removeFieldCard(int idx){
         summonedCards.remove(idx);
         cardInfo.remove(idx);
@@ -174,6 +266,10 @@ public class Player {
         FieldView.initField(this);
     }
 
+    /**
+     * delete skill card
+     * @param idx index card on the field
+     */
     public void destroyUsedAt(int idx){
         for (int i : State.p1.summonedCards.keySet()){
             if (State.p1.summonedCards.get(i) instanceof Skill){
@@ -193,6 +289,11 @@ public class Player {
         }
     }
 
+    /**
+     * 
+     * @param idx
+     * @return
+     */
     public boolean canBattle(int idx){
         return cardInfo.get(idx).getKey();
     }
